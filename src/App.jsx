5092bd16ec04e15260b7b1c137abc1d990c16f3e
darminnn <20240170@itla.edu.do>
2025-12-09 18:57:24 -0400

    feat(JIRA-000): Inicialización del proyecto con React, Vite, Tailwind CSS y Firebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500'>Tech Pro 200</h1>
+      <p className="read-the-docs">
+        Aquí irá el contenido
+      </p>
+    </>
+  )
+}
+
+export default App
